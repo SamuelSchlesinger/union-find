93ae624f7f9a674e9ec82676bbd81618944dc35f
Samuel Schlesinger <sgschlesinger@gmail.com>
2022-03-22 19:05:27 -0400

    Added some documentation at the top level of the module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! # Union Find
+//!
+//! `union-find` is an implementation of the [disjoint-set
+//! data structure](https://en.wikipedia.org/wiki/Disjoint-set_data_structure)
+//! of the same name.
+//!
+//! The interface to this data structure is composed of two functions. [`find`]
+//! allows you to compute a canonical representative for a set, given an element
+//! of it. [`union`] allows you to combine two sets, given an arbitrary representative
+//! from each. In the future, if you were to [`find`] a canonical representative
+//! from any member of either set, you will find them equal, indicating they are in
+//! the same set according to the data structure.
+//!
+//! The interface can be used like this:
+//!
+//! ```
+//! let mut uf = UnionFind::new(10);
+//!
+//! assert_eq!(uf.find(0).unwrap(), 0);
+//!
+//! let rep = uf.union(0, 1).unwrap();
+//!
+//! assert_eq!(rep, uf.find(1).unwrap());
+//! ```
+
 /// A [`UnionFind`] structure allows you to maintain items
 /// indexed by natural numbers, each in a disjoint set.
 pub struct UnionFind {
